b5cdb36bc4f1430a8864180cddf49c524db92c85
AlxOtsu <workunit1368@gmail.com>
2023-08-13 18:10:19 +0300

    add post question template

--- front/sending_a_questionnaire.js
+++ front/sending_a_questionnaire.js
@@ -1,16 +1,13 @@
-function sendDataToServer(data) {
-	// Опции для POST-запроса
-	const requestOptions = {
-		method: "POST",
-		headers: {
-			"Content-Type": "application/json",
-		},
-		body: JSON.stringify(data),
-	};
+const host = "http://localhost:5000/"
+const post_nanny_form = document.getElementById("post_nanny")
 
-	// Выполняем POST-запрос с помощью Fetch API
-	fetch("http://localhost:5000/nanny", requestOptions)
-		.then((response) => {
+function post_nanny(){
+	fetchOptions = {
+		method: "POST",
+		body: new FormData(post_nanny_form)
+	}
+
+	fetch(host + "nanny", fetchOptions).then((response) => {
 			if (!response.ok) {
 				throw new Error("Ошибка запроса: " + response.status);
 			}
@@ -25,39 +22,11 @@ function sendDataToServer(data) {
 			// Обработка ошибки
 			console.error(error.message);
 		});
+
 }
 
-// Обработчик события для кнопки "Отправить"
-document
-	.getElementById("sending_a_babysitter_questionnaire")
-	.addEventListener("click", function () {
-		// Получаем данные из формы
-		const name = document.getElementById("nameForm").value;
-		const image = document.querySelector(".image_nanny").files[0]; // Вам нужно обработать загрузку изображения вручную, если необходимо
-		const selection = document.querySelector(".form-select").value;
-		const phone = document.getElementById("phoneForm").value;
-		const preferredContactMethod = document.getElementById("flexRadioDefault1")
-			.checked
-			? "Мобильная связь"
-			: "Мессенджер";
-		const username = document.querySelector(".input-group input").value;
-		const email = document.getElementById("emailForm").value;
-		const birthDate = document.querySelector('input[name="calendar"]').value;
-		const additionalComments = document.getElementById("commentsForm").value;
+post_nanny_form.addEventListener('submit', post_nanny);
 
-		// Создаем объект для отправки данных
-		const data = {
-			name,
-			image, // Вам нужно обработать загрузку изображения вручную, если необходимо
-			selection,
-			phone,
-			preferredContactMethod,
-			username,
-			email,
-			birthDate,
-			additionalComments,
-		};
-
-		// Выполняем POST-запрос
-		sendDataToServer(data);
-	});
+// Осталось сделать
+// Обработка результатов запроса (10 строка)
+// Чтобы фориа после запроса не обновлялась
